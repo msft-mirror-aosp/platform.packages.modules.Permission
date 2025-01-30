fa10363d1d78345cfdeabe9612d968ac8d197a94
Vladimir Komsiyski <vladokom@google.com>
2025-01-29 23:18:49 -0800

    Remove allowBypassingQualifications from the VDM roles.
    Starting Android 16 no longer needed as CTS rely on Shell.
    
    The tests can use the Shell identity and the new CDM test API to
    acquire the needed permissions and create a virtual device
    
    Fix: 391599609
    Test: atest
    Flag: EXEMPT test improvement
    LOW_COVERAGE_REASON=NON_CODE_ONLY
    Relnote: N/A
    Change-Id: I4d61f2c5d086d9eeb7393ea18c6693d30b26eb57

--- PermissionController/role-controller/java/com/android/role/controller/behavior/v33/CompanionDeviceAppStreamingRoleBehavior.java
+++ PermissionController/role-controller/java/com/android/role/controller/behavior/v33/CompanionDeviceAppStreamingRoleBehavior.java
@@ -20,10 +20,12 @@ import android.content.Context;
 import android.os.UserHandle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.android.role.controller.model.Role;
 import com.android.role.controller.model.RoleBehavior;
 import com.android.role.controller.util.NotificationUtils;
+import com.android.role.controller.util.RoleFlags;
 import com.android.role.controller.util.UserUtils;
 
 /**
@@ -45,5 +47,11 @@ public class CompanionDeviceAppStreamingRoleBehavior implements RoleBehavior {
         if (!UserUtils.isManagedProfile(user, context)) {
             NotificationUtils.revokeNotificationAccessForPackageAsUser(packageName, user, context);
         }
+    }
+
+    @Override
+    @Nullable
+    public Boolean shouldAllowBypassingQualification(@NonNull Role role, @NonNull Context context) {
+        return !RoleFlags.isAtLeastB();
     }
 }
